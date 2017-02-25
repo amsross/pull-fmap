20eb5c2ec1b66abf5d5d982fad06f7d2c4d540d0
Matt Ross <amsross@gmail.com>
2017-02-25 01:51:38 -0500

    Don't duplicate pull-stream/throughs/map code

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 'use strict';
 
-function id (e) { return e; };
-const prop = require('pull-stream/util/prop');
+const map = require('pull-stream/throughs/map');
 
 module.exports = function fmap (mapper) {
-  if (!mapper) return id;
-  mapper = prop(mapper);
-  return function (read) {
-    return function (abort, cb) {
-      read(abort, function (end, data) {
-        try {
-          data = !end ? data.map(mapper) : null;
-        } catch (err) {
-          return read(err, function () {
-            return cb(err);
-          });
-        }
-        cb(end, data);
-      });
-    };
-  };
+  return map(x => {
+    if (typeof x.map !== 'function') throw new Error(`${x} is not a functor`);
+    return x.map(mapper);
+  });
 };
--- test/index.js
+++ test/index.js
@@ -15,14 +15,12 @@ class Nothing {
   map (x) { return this.of(); }
 };
 
-test('map throughs ends stream', function (t) {
+test('fmap throughs ends stream', function (t) {
   const err = new Error('unwholesome number');
   pull(
     pull.values([
-      new Nothing(),
-      new Just(1),
-      new Just(3),
-      new Just(3.4)
+      [1, 2, 3],
+      [3.4, 4],
     ]),
     fmap(function (e) {
       if (e !== ~~e) throw err;
@@ -33,6 +31,17 @@ test('map throughs ends stream', function (t) {
       if (data instanceof Nothing) t.notOk(data.value, 'Nothing has no value');
     }, _err => {
       t.equal(_err, err);
+      t.end();
+    })
+  );
+});
+
+test('fmap throws on non-functor values', function (t) {
+  pull(
+    pull.values([1]),
+    fmap(x => x),
+    pull.drain(null, _err => {
+      t.deepEqual(_err, new Error('1 is not a functor'), 'expected error was thrown');
       t.end();
     })
   );
